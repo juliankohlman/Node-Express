dbb68cd0ed4b7defbbf021b5ed8a29e60cd70a02
Julian Kohlman <juliankohlman@gmail.com>
2018-02-12 21:12:31 -0500

    Updated server requests code

--- server.js
+++ server.js
@@ -6,36 +6,34 @@ const PORT = 3030;
 
 
 app.use(bodyParser.json());
+// create a users array
 const users = [];
 let userId = 0;
 
-app.use('/', (req, res, next) => {
-  console.log('Req type:', req.method);
-  next();
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 })
 
-app.get('/', (req, res, next) => {
-  res.send('Hello word');
-  next();
-})
-
-///// user paths
+app.post('/users/:name/:id', (req, res) => {
+  users.push({name: req.params.name, id: req.params.id})
+  res.send(users);
+});
 
 app.get('/users', (req, res) => {
-  res.send(users.join('-'));
+  res.send(users);
 })
 
-app.get('/users/:userId', (req, res) => {
-  res.send(users[req.params.userId] + "" || "User id does not exist");
+app.get('/users/:id', (req, res) => {
+  res.send(users.filter(user => user.id === req.params.id));
 });
 
-app.post('/users', (req, res) => {
-  userId ++;
-  users.push(userId)
-  res.send(userId + "");
-});
+app.get('/search', (req, res) => {
 
+})
 
+app.delete('/users/:id', (req, res) => {
+  res.send(users.filter(user => user.id !== req.params.id));
+})
 
 app.listen(PORT, () => {
   console.log('App is listening on port 3030!');
